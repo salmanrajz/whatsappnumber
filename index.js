9f8251d3b6c1434a9a9d5cf898caa7e1d63d1bf8
salman <salmanahmed334@gmail.com>
2022-02-10 13:41:14 +0500

    Latest Update

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const fs = require("fs");
 const multer = require('multer');
 const csv = require('csv-parser');
 const makeCsv = require('csv-writer');
+var request = require('request');
 var ProgressBar = require('progress');
+var nodemailer = require('nodemailer');
 const { redirect } = require("express/lib/response");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -79,6 +81,7 @@ class App {
     console.log(`${chalk.green("✓")} Whatsapp Connection is Open`)
     console.log(`${chalk.green("✓")} Ready - using account of: ${this.client.user.name}`)
 
+    
 
 
 
@@ -87,84 +90,139 @@ class App {
      var data = [];
      var fileNamedate = Date.now();
 
-     try {
-        var file = fs.readdirSync("./uploads", {
-          withFileTypes: true
-        })
-        file.forEach((e) => {
-          if (e.name === req.file.originalname) {
-            fs.createReadStream(__dirname + "/uploads/" + e.name)
-              .pipe(csv())
-              .on('data', async function (row) {
-                await Promise.all(
-                  Object.keys(row).map(async (v, k) => new Promise((resolve, reject) => {
-                    if (!isNaN(row[v] || row[v] !== '')) {
-                      setTimeout(async () => {
-                        const contactId = row[v].includes('@s.whatsapp.net') ? row[v].trim() : row[v].trim() + '@s.whatsapp.net'
-                        const result = await clite.isOnWhatsApp(contactId)
-                        if (result == undefined) {
-                          console.log("Google");
-                          data.push({
-                            number: row[v],
-                            exits: false
-                          })
-                        } else {
-                          console.log("Success", result, row[v]);
-                          data.push({
-                            number: row[v],
-                            exits: result.exists
-                          })
-                        }
-                        resolve()
-                      }, k * 100)
-                    }
-                  })));
-                if (data.length > 0) {
-                  // console.log(data)
-                  
-                    const file = makeCsv.createObjectCsvWriter({
-                      path: __dirname + "/newFile/" + fileNamedate + "WhatsAppVerifier.csv",
-                      header: [{
-                          id: "number",
-                          title: 'Number'
-                        },
-                        {
-                          id: "exits",
-                          title: 'Exits'
-                        }
-                      ]
-                    })
+    // 
+    request.post(
+      'https://crmapi.test/api/CheckAuth', {
+        json: {
+          // FileName: fileNamedate + "WhatsAppVerifier.csv",
+          email: req.body.email,
+          // device_name: 'value',
+        }
+      },
+      function (error, response, body) {
+        // if (!error && response.statusCode == 200) {
+        // console.log(body);
+        // }
+        // console.log(error)
+        // console.log(response)
+        // console.log(body)
+        //  global.Auth = body;
+        // global.number
+        var Auth = body;
+        var a =   Auth.split(',');
+        // a.1;a[1]
+        // a[1]
+         if (a[0] == 1) {
+
+           // 
+           try {
+             var file = fs.readdirSync("./uploads", {
+               withFileTypes: true
+             })
+             file.forEach((e) => {
+               if (e.name === req.file.originalname) {
+                 fs.createReadStream(__dirname + "/uploads/" + e.name)
+                   .pipe(csv())
+                   .on('data', async function (row) {
+                     await Promise.all(
+                       Object.keys(row).map(async (v, k) => new Promise((resolve, reject) => {
+                         if (!isNaN(row[v] || row[v] !== '')) {
+                           setTimeout(async () => {
+                             const contactId = row[v].includes('@s.whatsapp.net') ? row[v].trim() : row[v].trim() + '@s.whatsapp.net'
+                             const result = await clite.isOnWhatsApp(contactId)
+                             if (result == undefined) {
+                               // console.log("Google");
+                               data.push({
+                                 number: row[v],
+                                 exits: false
+                               })
+                             } else {
+                               console.log("Success", result, row[v]);
+                               data.push({
+                                 number: row[v],
+                                 exits: result.exists
+                               })
+                             }
+                             resolve()
+                           }, k * 100)
+                         }
+                       })));
+                       console.log(data.length)
+                     if (data.length < a[1]) {
+                      //  console.log("300 sy agay mahi jany ka" + data.length)
+                       const file = makeCsv.createObjectCsvWriter({
+                         path: __dirname + "/newFile/" + fileNamedate + "WhatsAppVerifier.csv",
+                         header: [{
+                             id: "number",
+                             title: 'Number'
+                           },
+                           {
+                             id: "exits",
+                             title: 'Exits'
+                           }
+                         ]
+                       })
+
+                       file
+                         .writeRecords(data)
+                         .then(() => {
+                          //  console.log(data.length)
+                         });
+
+                     }
+                     else if(data.length === a[1]){
+                       console.log("300 Limit Cross")
+                      res.render('alert.ejs')
+                     }else{
+                      console.log("WOW AMAJING")
+                     }
                    
-                    file
-                      .writeRecords(data)
-                      .then(() => {
-                   
-                  });
-                 
-                }
-                console.log("END OF THE DATE")
-                //  res.download(__dirname + "/newFile/" + fileNamedate + "WhatsAppVerifier.csv")
-              })
-              .on('end', function () {
-                console.table("users")
-                //  res.download(__dirname + "/newFile/" + fileNamedate + "WhatsAppVerifier.csv")
-              })
-          }
-          
-        })
-        // res.redirect('/')
-         res.render("index.ejs", {
-           result: [{
-             number:"923111312164",
-             value: 
-             fileNamedate + "WhatsAppVerifier.csv",
-           }]
-         })
-     } catch (error) {
-         return res.render("index.ejs", {
-           error: 'nomernya ada yang salah tu'
-         })
-    }
+                     //  res.download(__dirname + "/newFile/" + fileNamedate + "WhatsAppVerifier.csv")
+                   })
+                   .on('end', function () {
+                     // console.table("users")
+                     //  res.download(__dirname + "/newFile/" + fileNamedate + "WhatsAppVerifier.csv")
+                   })
+               }
+
+             })
+            //  console.log(req.body.email)
+              // res.send(``)
+             request.post(
+               'https://crmapi.test/api/SecretApi', {
+                 json: {
+                   FileName: fileNamedate + "WhatsAppVerifier.csv",
+                   email: req.body.email,
+                   // device_name: 'value',
+                 }
+               },
+               function (error, response, body) {
+                 // if (!error && response.statusCode == 200) {
+                 // console.log(body);
+                 // }
+                 // console.log(error)
+                 // console.log(response)
+                 console.log(body)
+               }
+             );
+             res.render('alert.ejs');
+        
+           } catch (error) {
+             return res.render("index.ejs", {
+               error: 'Kindly Upload File First'
+             })
+           }
+         } else {
+           return res.render("index.ejs", {
+             error: 'License Expired or Invalid'
+           })
+         }
+      }
+    );
+    // console.log
+    // var Auth = 0;
+   
+
     })
 
 
@@ -200,7 +258,7 @@ class App {
               result ? console.log(`[${chalk.green(i)}] Number:`, number, '| Result:', result) : console.log(`[${chalk.red(i)}] Number:`, number, '| Result:', result)
               console.log(result)
               if (result == undefined) {
-                console.log("Google");
+                // console.log("Google");
                 validNumber.push({
                   number,
                   value: ", Failed"
